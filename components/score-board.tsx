4f714c09f676278fbb8ad3020afb5e63e7579452
fbocolowski <fbocolowski@icloud.com>
2024-10-28 12:26:26 -0300

    Removed unused imports

--- components/score-board.tsx
+++ components/score-board.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import { Clock } from "lucide-react";
 import { ScoreCard } from "./score-card";
 import { ScoreClock } from "./score-clock";
 
